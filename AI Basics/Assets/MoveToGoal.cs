deb255c68db62257e80a373b354a06410a28649e
Purvang-Acharya <66854326+Purvang-Acharya@users.noreply.github.com>
2020-07-21 10:45:37 +0530

    Learning vectors

---
+++ AI Basics/Assets/MoveToGoal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToGoal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 2.0f;
+    public Transform goal;
+    public float accuracy = 0.01f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 direction = goal.position - this.transform.position;
+        if(direction.magnitude>accuracy)
+        this.transform.Translate(direction.normalized * speed * Time.deltaTime);
+    }
+}
