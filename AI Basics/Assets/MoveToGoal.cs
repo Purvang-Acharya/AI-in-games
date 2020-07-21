e5f70a720b7a3e8f5a7ff05a9904b6de5e2b3e27
Purvang-Acharya <66854326+Purvang-Acharya@users.noreply.github.com>
2020-07-21 13:30:36 +0530

    look at goal

--- AI Basics/Assets/MoveToGoal.cs
+++ AI Basics/Assets/MoveToGoal.cs
@@ -10,14 +10,16 @@ public class MoveToGoal : MonoBehaviour
     public float accuracy = 0.01f;
     void Start()
     {
-        
+        this.transform.LookAt(goal.position);
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         Vector3 direction = goal.position - this.transform.position;
+        this.transform.LookAt(goal.position);
+        Debug.DrawRay(this.transform.position, direction, Color.cyan);
         if(direction.magnitude>accuracy)
-        this.transform.Translate(direction.normalized * speed * Time.deltaTime);
+        this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
     }
 }
