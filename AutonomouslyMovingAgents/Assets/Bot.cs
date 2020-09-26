e471110656f7f0884cc88c0794e06a4cda872a19
Purvang-Acharya <66854326+Purvang-Acharya@users.noreply.github.com>
2020-09-26 17:39:52 +0530

    flee and seek

---
+++ AutonomouslyMovingAgents/Assets/Bot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Bot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    NavMeshAgent agent;
+    public GameObject target;
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Seek(Vector3 location) 
+    {
+        agent.SetDestination(location);
+    }
+    void Flee(Vector3 location)
+    {
+        Vector3 vectDirection = location - this.transform.position;
+        agent.SetDestination(this.transform.position - vectDirection);
+    }
+    void Update()
+    {
+        //Seek(target.transform.position);
+        Flee(target.transform.position);
+    }
+}
