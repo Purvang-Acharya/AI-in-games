eff740320aa6cfe6142d54574775fc7bbcaad23d
Purvang-Acharya <66854326+Purvang-Acharya@users.noreply.github.com>
2020-09-27 16:09:55 +0530

    added Pursuit and Evade

--- AutonomouslyMovingAgents/Assets/Bot.cs
+++ AutonomouslyMovingAgents/Assets/Bot.cs
@@ -23,9 +23,39 @@ public class Bot : MonoBehaviour
         Vector3 vectDirection = location - this.transform.position;
         agent.SetDestination(this.transform.position - vectDirection);
     }
+
+    void Pursuit()
+    {
+        Vector3 targetDirection = target.transform.position - this.transform.position;
+        float relAngle = Vector3.Angle(this.transform.forward, this.transform.TransformVector(target.transform.forward));
+        float targetAngle = Vector3.Angle(this.transform.forward, this.transform.TransformVector(targetDirection));
+        if ((targetAngle>90 && relAngle<20)||target.GetComponent<Drive>().currentSpeed < 0.01f)
+        {
+            Seek(target.transform.position);
+            return;
+        }
+        float lookAhead = targetDirection.magnitude/(agent.speed + target.GetComponent<Drive>().currentSpeed);
+        Seek(target.transform.position + target.transform.forward * lookAhead);
+
+    }
+    void Evade()
+    {
+        Vector3 targetDirection = target.transform.position - this.transform.position;
+       /*float relAngle = Vector3.Angle(this.transform.forward, this.transform.TransformVector(target.transform.forward));
+        float targetAngle = Vector3.Angle(this.transform.forward, this.transform.TransformVector(targetDirection));
+        if ((targetAngle > 90 && relAngle < 20) || target.GetComponent<Drive>().currentSpeed < 0.01f)
+        {
+            Seek(target.transform.position);
+            return;
+        }*/
+        float lookAhead = targetDirection.magnitude / (agent.speed + target.GetComponent<Drive>().currentSpeed);
+        Flee(target.transform.position + target.transform.forward * lookAhead);
+    }
     void Update()
     {
         //Seek(target.transform.position);
-        Flee(target.transform.position);
+        //Flee(target.transform.position);
+        //Pursuit();
+        Evade();
     }
 }
